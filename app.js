f69b2c652e359a36c46b5ecb250986ba25bf22e6
drdevlin <drdevlin@fastmail.com>
2021-01-05 22:12:13 -0500

    make functional

--- app.js
+++ app.js
@@ -3,14 +3,30 @@ const https = require('https');
 const express = require('express');
 const app = express();
 
+function rebuildQuery(queryObject) {
+  const queryObjectEntries = Object.entries(queryObject).filter(entry => entry[0] !== 'url');
+  let queryString = '?';
+  let ranOnce = false;
+  for (const [key, value] of queryObjectEntries) {
+    if (ranOnce) queryString += '&';
+    if (key !== 'url') queryString += `${key}=${value}`;
+    ranOnce = true;
+  }
+  return queryString;
+}
+
 app.get('/', (req, res) => {
   const url = req.query.url;
+  const queryString = rebuildQuery(req.query);
   if (url) {
     let data = '';
-    https.get(url, response => {
+    https.get(url + queryString, response => {
       response.on('data', chunk => data += chunk);
-      response.on('close', () => res.send(data));
-    })
+      response.on('close', () => {
+        res.set('Access-Control-Allow-Origin', process.env.WHITELIST);
+        res.send(data);
+      });
+    });
   }
 });
 
