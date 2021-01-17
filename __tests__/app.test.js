7b559f3a055939ca0d3448047daaec09989733a1
drdevlin <drdevlin@fastmail.com>
2021-01-17 13:12:38 -0500

    Add initial integration test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,10 +2,21 @@ const app = require('../app.js');
 const supertest = require('supertest');
 const request = supertest(app);
 
+const https = require('https');
+jest.mock('https');
+https.get.mockImplementation((string1, callback1) => {
+  const mockListener = {
+    on: (string2, callback2) => {
+      if (string2 === 'close') callback2();
+    }
+  }
+  callback1(mockListener);
+});
+
 describe('app.js', () => {
   describe('GET /', () => {
-    it('status 200', async done => {
-      const response = await request.get('/?url=https://daringfireball.net');
+    it('responds 200 with a <url> query', async (done) => {
+      const response = await request.get('/?url=something');
       expect(response.status).toBe(200);
       done();
     });
